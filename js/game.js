335f10e88afd667666a991d440bdb18beec63b73
Souradeep <souradeepkrbasu@gmail.com>
2020-04-11 04:50:31 +0530

    Update

--- js/game.js
+++ js/game.js
@@ -81,7 +81,7 @@ getNewQuestion = () => {
   choices.forEach(choice => {
     const number = choice.dataset["number"];
     choice.innerText = currentQuestion["choice" + number];
-  }), 400});
+  }), 600});
 
   availableQuesions.splice(questionIndex, 1);
   acceptingAnswers = true;
